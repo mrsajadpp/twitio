96ae1dca658666724aea8a8ce43a48adb00ef7aa
mrsajadpp <saju4753@gmail.com>
2023-07-23 14:36:48 +0530

    done

--- routes/download.js
+++ routes/download.js
@@ -7,28 +7,30 @@ const credentials = {
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET
 }
-
+ 
 const spotify = new Spotify(credentials); 
 
 /* GET users listing. */
 router.post('/youtube', function (req, res, next) {
   try {
-    res.header('Content-Disposition', 'attachment; filename="YouTube - ' + new Date() + '- twitio.thintry.com.mp4"');
     ytdl(req.body.url, { format: 'mp4', filter: 'audioandvideo', quality: 'highest' }).pipe(res);
+    res.header('Content-Disposition', 'attachment; filename="YouTube - ' + new Date() + '- twitio.thintry.com.mp4"');
     res.end()
   } catch (error) {
-    res.status(500).send('500')
+    res.send(error.message)
   }
 });
 
 router.post('/spotify', async (req, res, next) => {
   try {
-    res.header('Content-Disposition', 'attachment; filename="Spotify - ' + new Date() + ' - twitio.thintry.com.mp3"');
     let song = await spotify.downloadTrack(req.body.url);
+    res.header('Content-Disposition', 'attachment; filename="Spotify - ' + new Date() + ' - twitio.thintry.com.mp3"');
+    console.log(song)
     res.write(song, 'binary');
     res.end();
   } catch (error) {
-    res.status(500).send('500')
+    console.log(error)
+    res.send(error.message)
   }
 })
 
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { description: "Download video's from your favourite social media" });
 });
 
+router.get('/youtube', (req, res, next) => {
+  res.render('youtube', { title: "YouTube Video Downloader", description: "Download YouTube video with twitio" })
+})
+
+router.get('/spotify', (req, res, next) => {
+  res.render('spotify', { title: "Spotify Song Downloader", description: "Download your favourite spotify song with twitio" })
+})
+
 module.exports = router; 
